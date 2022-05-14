974441357b8e0c50fb8af97c91b0ab2d5c529cdd
Alejandro <alejandroverah97@gmail.com>
2022-05-14 14:10:22 -0500

    Setup blogpost

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	import Main from '../components/Main.svelte';
+
+	export let segment;
+</script>
+
+
+
+<Nav {segment}/>
+
+<Main>
+	<slot></slot>
+</Main>
