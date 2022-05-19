0c14408a025490fd55bf730ea5c4dc566d774266
Alejandro <alejandroverah97@gmail.com>
2022-05-18 20:17:57 -0500

    Full integration backend

--- src/routes/general.svelte
+++ src/routes/general.svelte
@@ -1,7 +1,40 @@
+<script context="module">
+	export function preload({ params, query }) {
+		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
+			return { posts };
+		});
+	}
+</script>
+
+<script>
+    import Post from '../components/Post.svelte';
+    export let posts;
+
+    const filterPost = posts.filter(post => post.tag === 'general');
+</script>
+
+<style>
+    .Post {
+        display: grid;
+        justify-content: space-between;
+        gap: 30px;
+        grid-template-columns: 1fr;
+    }
+</style>
+
 <svelte:head>
     <title>General</title>
 </svelte:head>
 
 <div class="General">
-    <h2>General</h2>
+    <h1>General</h1>
+    <div class="Posts">
+        {#if filterPost >= 1}
+            {#each filterPost as post}
+                <Post {post} />
+            {/each}
+        {:else}
+            <p>No results...</p>
+        {/if}
+    </div>
 </div>
